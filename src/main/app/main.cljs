120f84a830664164322536083d77927210041ec8
Christoph Frick <cf@ofnir.net>
2020-06-02 23:58:55 +0200

    Only include the antd button

--- src/main/app/main.cljs
+++ src/main/app/main.cljs
@@ -1,9 +1,9 @@
 (ns app.main
   (:require [rum.core :as rum]
-            ["antd" :as antd]))
+            ["antd/es/button" :default Button]))
 
 (rum/defc component []
-  (rum/adapt-class antd/Button {:onClick (fn [e] (println "Clicked"))} "Click me"))
+  (rum/adapt-class Button {:onClick (fn [e] (println "Clicked"))} "Click me"))
 
 (defn root []
   (rum/mount (component) (js/document.getElementById "app")))
