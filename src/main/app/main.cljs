b3be33b46a23d531ac4a358b7f2688de6151cc91
Christoph Frick <cf@ofnir.net>
2020-06-02 23:46:00 +0200

    Add rum and antd
    Switch out the cljsjs deps of rum to use the locally installed react/react-dom
    and add an antd/Button to the example page

--- src/main/app/main.cljs
+++ src/main/app/main.cljs
@@ -1,7 +1,17 @@
-(ns app.main)
+(ns app.main
+  (:require [rum.core :as rum]
+            ["antd" :as antd]))
+
+(rum/defc component []
+  (rum/adapt-class antd/Button {:onClick (fn [e] (println "Clicked"))} "Click me"))
+
+(defn root []
+  (rum/mount (component) (js/document.getElementById "app")))
 
 (defn main! []
-  (println "[main]: loading"))
+  (println "[main]: loading")
+  (root))
 
 (defn reload! []
-  (println "[main]: reloading"))
+  (println "[main]: reloading")
+  (root))
